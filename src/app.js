3f82c0d7622724cd335c54ec94dca5fde28a6a92
siyam79 <siyamahmed3827@gmail.com>
2024-11-30 16:25:59 +0600

    cors origin update

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ app.use(express.json());
 
 // cors origin
 const corsOptions = {
-  origin: ["*", "http://localhost:5173", "http://localhost:5174"],
+  // origin: ["*", "http://localhost:5173", "http://localhost:5174"],
+  origin: ["*", "https://core-web-bd-front-end.vercel.app", "https://core-web-bd-front-end.vercel.app"],
   credentials: true,
   optionSuccessStatus: 200,
 };
