4bc527af3a22b1ac634abf5d81119a8daf59afa5
siyam79 <siyamahmed3827@gmail.com>
2024-11-30 17:22:43 +0600

    some error fixed

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 // parsers
 app.use(express.json());
 
-app.use(cors(corsOptions));
+
 // cors origin
 const corsOptions = {
   // origin: ["*", "http://localhost:5173", "http://localhost:5174"],
@@ -16,7 +16,7 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
-
+app.use(cors(corsOptions));
 
 // application routes
 app.use('/api/v1', UsersRoutes);
